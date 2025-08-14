da0912824beb9f768c4ef45cd8e0eccf09fe28c4
miura671 <mi0223ha@icloud.com>
2025-08-14 23:17:32 +0900

    Delete 4-20 directory

--- 4-20/4-20_2/app/src/main/java/_2/Divider.java
+++
@@ -1,9 +0,0 @@
-package  _2;
-public class Divider {
-    public double divide(double a, double b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("ゼロで除算はできません");
-        }
-        return a / b;
-    }
-}
