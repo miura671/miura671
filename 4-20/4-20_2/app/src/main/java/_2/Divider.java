1448adb535a6070b29feb3a25f5fe408caa256a1
miura671 <mi0223ha@icloud.com>
2025-08-14 23:11:04 +0900

    Add files via upload

---
+++ 4-20/4-20_2/app/src/main/java/_2/Divider.java
@@ -0,0 +1,9 @@
+package  _2;
+public class Divider {
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("ゼロで除算はできません");
+        }
+        return a / b;
+    }
+}
